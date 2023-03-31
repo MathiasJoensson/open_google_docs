2c0836dce6c326f80cf1f3673f82fe20fe66dd45
MathiasJoensson <ironmatte@hotmail.com>
2023-03-31 14:59:03 +0200

    Script to automatically open all Google docs files in a specific folder in My Drive that is synced to the local computer

---
+++ google_docs.py
@@ -0,0 +1,21 @@
+## Open all Google Docs in a folder from Drive synced to local machine
+
+import os
+import webbrowser
+
+# Specify the folder ID of the folder containing the Google Docs
+FOLDER_ID = "ORs"
+
+# Specify the path to the folder containing the Google Docs
+folder_path = "G:/My Drive/" + FOLDER_ID
+
+# Check if the folder exists
+if not os.path.isdir(folder_path):
+    print("Error: Google Drive folder not found at", folder_path)
+    exit()
+
+# Open all Google Docs files in the folder in the default browser
+for file in os.listdir(folder_path):
+    if file.endswith(".gdoc"):
+        file_path = folder_path + "/" + file
+        webbrowser.open(file_path)
